02a9dc67d6f7741944d2fa557ad063ab04824193
rajatsharma <rajat.almighty@gmail.com>
2017-08-10 02:45:18 +0530

    Added text to console window

--- CompareTheMarketTEST/Program.cs
+++ CompareTheMarketTEST/Program.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CompareTheMarketTEST
 {
@@ -14,21 +11,31 @@ namespace CompareTheMarketTEST
     {
         static void Main(string[] args)
         {
-
+            
             string inputFilePath = ConfigurationManager.AppSettings["inputFile"];
             string outputFilePath = ConfigurationManager.AppSettings["outputFile"];
 
+            Console.WriteLine("Started processing the file....Please wait, this may take while...");
             var wordsInBook = ProcessFileToGetWords(inputFilePath);
+            Console.WriteLine("Word count Complete!{0}", Environment.NewLine);
 
+            Console.WriteLine("Determining if the count of the word is prime...");
             //Determine if the word count is prime
             foreach (var word in wordsInBook)
             {
                 word.IsPrime = Helper.IsNumberPrime(word.Count);
             }
+            Console.WriteLine("Finished checking if the count of the word is prime!{0}", Environment.NewLine);
+            
 
+            Console.WriteLine("Started writing to output csv file...");
             //Write the output to a csv file
             Helper.WriteCSV(wordsInBook, outputFilePath);
-            
+            Console.WriteLine("Finished writing to file!{0}", Environment.NewLine);
+
+            Console.WriteLine("Press enter to continue. The output file will be in the \"Data\" folder.");
+
+            Console.Read();
         }
 
         
@@ -39,8 +46,9 @@ namespace CompareTheMarketTEST
         /// </summary>
         /// <param name="inputFilePath"></param>
         /// <returns></returns>
-        public static List<WordInfo> ProcessFileToGetWords(string inputFilePath)
+        public static IEnumerable<WordInfo> ProcessFileToGetWords(string inputFilePath)
         {
+            
             var wordsInBook = new List<WordInfo>();
             using (FileStream fs = File.Open(inputFilePath, FileMode.Open, 
                 FileAccess.Read, FileShare.ReadWrite))
@@ -69,12 +77,11 @@ namespace CompareTheMarketTEST
                             var sanitizedWord = Helper.SanitizeWord(word);
 
                             //Check if a word already exists in the list in memory so far
-                            var x = wordsInBook.Where(w => w.Word == sanitizedWord.ToLowerInvariant())
-                                .ToList()
+                            var wordRef = wordsInBook.Where(w => w.Word == sanitizedWord.ToLowerInvariant())
                                 .SingleOrDefault();
 
                             //If the word appeared for the first time, add it to the list object
-                            if (x == null)
+                            if (wordRef == null)
                             {
                                 wordsInBook.Add(new WordInfo
                                 {
@@ -86,7 +93,7 @@ namespace CompareTheMarketTEST
                             //If word already exists, just increase the count of that word
                             else
                             {
-                                x.Count += 1;
+                                wordRef.Count += 1;
                             }
                         }
                     }
@@ -99,7 +106,9 @@ namespace CompareTheMarketTEST
 
                 }
             }
-            return wordsInBook;
+
+            //Arrange by highest count first
+            return wordsInBook.OrderByDescending(x => x.Count);
         }
 
         
